11ba17af0a48c41ca4afcc65739598a97cd04419
Stephen Thomas <sthomas1@eogresources.com>
2019-10-17 10:00:01 -0500

    Initial front end commit

---
+++ bravo_front-end/src/components/template.js
@@ -0,0 +1,22 @@
+/* Package imports */
+import React from "react";
+import styled from "styled-components";
+
+/* Component Imports */
+/* Stylesheet Imports */
+// import classes from "./Template.module.css";
+/* Styling */
+
+
+/* COMPONENT */
+const Template = props => {
+  /* STATE VARIABLES ------------------------------------------ */
+  /* HOOKS ---------------------------------------------------- */
+  /* METHODS -------------------------------------------------- */
+  /* HANDLERS ------------------------------------------------- */
+  /* CONDITIONAL SUB-COMPONENTS ------------------------------- */
+  /* RENDER --------------------------------------------------- */
+  return <div></div>;
+};
+
+export default Template;
